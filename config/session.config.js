11c1c8c26d03c0650a2087f6b630d21e9e49d422
Mich Seixas <mcseixas@gmail.com>
2023-05-11 12:11:35 +0200

    correción de typo

--- config/session.config.js
+++ config/session.config.js
@@ -11,7 +11,7 @@ module.exports = (app) => {
             sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
             secure: process.env.NODE_ENV === "production",
             httpOnly: true,
-            //maxAge: 60000   //temps de vida de la cookie i de l'id (ms)
+            //maxAge: 60000   tiempo de vida de la cookie y de la ID
         },
         store: mongoStore.create({mongoUrl:process.env.MONGODB_URI || "mongodb://localhost:27017/auth"})
     }));    
